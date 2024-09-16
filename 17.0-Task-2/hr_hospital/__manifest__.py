28609f135c928c6427e772681684d2c9be573103
vlad <vladio.ua@gmail.com>
2024-09-17 00:04:36 +0300

    task2

---
+++ 17.0-Task-2/hr_hospital/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'HR hospital',
+    'author': 'Vladislav',
+    'category': 'Customizations',
+    #'license': 'opl-1',
+    'version': '17.0.1.1.0',
+
+    'images': [
+        'static/description/logo.jpg'
+    ]
+}
