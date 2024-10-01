f9cc0464e83d6c08782fbf57590e1c79901fbe4b
vlad <vladio.ua@gmail.com>
2024-10-01 14:19:00 +0300

    task3.1

--- 17.0-Task-2/hr_hospital/models/hr_hospital_diseases.py
+++ 17.0-Task-2/hr_hospital/models/hr_hospital_diseases.py
@@ -1,7 +1,8 @@
 import logging
 from email.policy import default
 
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -19,3 +20,8 @@ class HrHospitalDiseases(models.Model):
         comodel_name='hr.hospital.visit',
         string="Visit",
     )
+
+    @api.onchange('approved')
+    def _onchange_approved(self):
+        if self.approved and self.hr_hospital_visit_id.hr_hospital_doctor_id.is_intern:
+            raise ValidationError('The intern cannot confirm the diagnosis.')
